227a69cef815f329d5e5c89b666e83959e82417e
Glauter Santos <glauterlima@gmail.com>
2025-05-07 09:38:47 -0300

    adição de filtros por tags

--- apps/galeria/urls.py
+++ apps/galeria/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.galeria.views import index, imagem, buscar
+from apps.galeria.views import index, imagem, buscar, filtro
 
 urlpatterns = [
     path('', index, name='index'),
     path('imagem/<int:foto_id>', imagem, name='imagem' ),
     path("buscar", buscar, name="buscar"),
+    path("filtro/<str:categoria>", filtro, name="filtro"),    
 ]
